de09e8b72d9d84fd92bca9f619550636bc26a608
Kyler Draper <kythegamedeveloper@gmail.com>
2020-04-14 13:42:14 -0600

    Added project 10.1

--- ch09-project2-finish/js/chapter09-project02.js
+++ ch09-project2-finish/js/chapter09-project02.js
@@ -4,24 +4,24 @@ window.addEventListener('load', function() {
         if(e.target.nodeName.toLowerCase() == 'img') {
             var clickImgSrc = e.target.src;
 
-            var newSrc = clickImgSrc.replace("small", "medium");
+            var newSrc = clickImgSrc.replace('small', 'medium');
 
-            var featuredImg = document.querySelector("#featured img");
+            var featuredImg = document.querySelector('#featured img');
             featuredImg.src = newSrc;
             featuredImg.title = e.target.title;
-            document.querySelector("figcaption").innerHTML = e.target.title;
+            document.querySelector('figcaption').innerHTML = e.target.title;
 
         }
     });
 
-   var img = document.getElementById('featured');
 
-   img.addEventListener("mouseover", function(e) {
+   var img = document.getElementById('featured');
+   img.addEventListener('mouseover', function(e) {
        img.lastElementChild.style.opacity = "80%";
        img.lastElementChild.style.transition = "1s";
    });
 
-   img.addEventListener("mouseout", function(e) {
+   img.addEventListener('mouseout', function(e) {
        img.lastElementChild.style.opacity = "0%";
        img.lastElementChild.style.transition = "1s";
     });
---
+++ ch10-project1-finish/README.md
@@ -0,0 +1,5 @@
+# Fundamentals of Web Development, 2nd Edition
+### Chapter 10 [JavaScript 3], Project 1 [Art]
+Use jQuery to respond to events and to programmatically modify HTML and CSS.
+
+  
